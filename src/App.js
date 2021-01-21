bfca6684ad4a30f206a7fa5b0bc53460b8f762eb
pin0S <pete.g.lynch@gmail.com>
2021-01-21 19:22:22 +1100

    glass styling added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    
+    <main>
+      <section class="dashboard">
+        <div class="sidebar">
+          <div class="user">
+                <img src={"none"} alt={'test'}/>
+                <p>User name</p>
+          </div>
+          </div>
+        <div class="content">
+            <h2>About Me</h2>
+        </div>
+      </section>
+    </main>
   );
 }
 
